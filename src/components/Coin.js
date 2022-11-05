312392c5538af71aa707a88f32d528fcdefffd19
Fábio Rodrigues <88008767+rochafrr@users.noreply.github.com>
2022-11-05 09:57:16 -0300

    Update Coin.js

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -9,15 +9,15 @@ function Coin({ TotalSupply, CirculatingSupply, Staking, BlockHeight, Burn }) {
         </div> 
    
         <div className="TSupply"> 
-          <h3>Total Supply ------------------------------- {TotalSupply} LUNC</h3> 
+          <h3>Total Supply ------------------------------- {(TotalSupply).toLocaleString('pt-br', {minimumFractionDigits: 2})} LUNC</h3> 
         </div> 
    
         <div className="CSupply"> 
-          <h3> Circ. Supply -------------------------------- {(TotalSupply)-(Staking/1000000)} LUNC</h3> 
+          <h3> Circ. Supply -------------------------------- {((TotalSupply)-(Staking/1000000)).toLocaleString('pt-br', {minimumFractionDigits: 2})} LUNC</h3> 
         </div> 
    
         <div className="Staking"> 
-          <h3>Pool Staking -------------------------------- {Staking/1000000} LUNC</h3> 
+          <h3>Pool Staking -------------------------------- {(Staking/1000000).toLocaleString()} LUNC</h3> 
         </div> 
    
         <div className="Block"> 
