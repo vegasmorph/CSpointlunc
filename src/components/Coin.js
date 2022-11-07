75813f7082e2f86c803b8a9d165827b3b46fe0bb
Vegas <103571823+vegasmorph@users.noreply.github.com>
2022-11-07 22:54:00 +0000

    Update Coin.js

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -31,28 +31,7 @@ function Coin({ TotalSupply, CirculatingSupply, Staking, BlockHeight, Burn }) {
 
         <div>
 
-          {Burn.map((item) => (
-            <table>
-              <thead>
-                <tr>
-                  <th className="id">ID</th>
-                  <th className="block">Block</th>
-                  <th className="amount">Amount</th>
-
-                </tr>
-              </thead>
-
-              <tbody>
-                <tr key={item.id}>
-                  <td>{item.chainId}</td>
-                  <td>{item.height}</td>
-                  <td>{(item.amount / 1000000).toFixed(2)} Lunc</td>
-                  
-
-                </tr>
-              </tbody>
-            </table>
-          ))}
+          
           </div>
         </div>
           
