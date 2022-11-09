f2c9df5bb95b68fa773be544d7493996f3549f38
Vegas <103571823+vegasmorph@users.noreply.github.com>
2022-11-09 15:23:32 +0000

    Update Coin.js

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -11,7 +11,7 @@ function Coin({ TotalSupply, CirculatingSupply, Staking, BlockHeight, Burn }) {
 
    
         <div className="CSupply"> 
-          <h3> Circ. Supply -------------------------------- {parseFloat((TotalSupply)-(Staking/1000000)).toLocaleString('pt-br', {minimumFractionDigits: 2})} LUNC</h3> 
+          <h3>  {parseFloat((TotalSupply)-(Staking/1000000)).toLocaleString('pt-br', {minimumFractionDigits: 2})}</h3> 
         </div> 
    
     
